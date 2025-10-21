8cb6af75f564afc500d36b17e96cca6c77ea1339
Таисия <taisiya.kryachko@gmail.com>
2025-10-21 10:45:25 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 
 import { generatePhotos } from './data.js';
+import { renderThumbnails } from './thumbnail.js';
+renderThumbnails();
 const photos = generatePhotos();
 // eslint-disable-next-line no-console
 console.log(photos);
