83b0d0d52fc62c2a2717428199555d61ca6a8be6
Таисия <taisiya.kryachko@gmail.com>
2025-11-01 21:34:02 +0300

    поправила

--- js/big-picture.js
+++ js/big-picture.js
@@ -7,31 +7,34 @@ const socialComments = bigPicture.querySelector('.social__comments');
 const socialCommentCount = bigPicture.querySelector('.social__comment-count');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const cancelButton = bigPicture.querySelector('.big-picture__cancel');
+const picturesContainer = document.querySelector('.pictures');
 
-const onDocumentKeydown = (evt) => {
+
+function onDocumentKeydown(evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
-    // eslint-disable-next-line no-use-before-define
     closeBigPicture();
   }
-};
+}
 
-const onOverlayClick = (evt) => {
+function onOverlayClick(evt){
   if (evt.target === bigPicture) {
-    // eslint-disable-next-line no-use-before-define
     closeBigPicture();
   }
-};
+}
+function closeBigPicture() {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  cancelButton.removeEventListener('click', closeBigPicture);
+  bigPicture.removeEventListener('click', onOverlayClick);
+}
 
 const createComment = (comment) => {
   const commentElement = document.createElement('li');
   commentElement.classList.add('social__comment');
   commentElement.innerHTML = `
-    <img
-      class="social__picture"
-      src="${comment.avatar}"
-      alt="${comment.name}"
-      width="35" height="35">
+    <img class="social__picture" src="${comment.avatar}" alt="${comment.name}" width="35" height="35">
     <p class="social__text">${comment.message}</p>
   `;
   return commentElement;
@@ -43,14 +46,6 @@ const renderComments = (comments) => {
     const commentElement = createComment(comment);
     socialComments.appendChild(commentElement);
   });
-};
-
-const closeBigPicture = () => {
-  bigPicture.classList.add('hidden');
-  body.classList.remove('modal-open');
-  document.removeEventListener('keydown', onDocumentKeydown);
-  cancelButton.removeEventListener('click', closeBigPicture);
-  bigPicture.removeEventListener('click', onOverlayClick);
 };
 
 const openBigPicture = (pictureData) => {
@@ -71,4 +66,22 @@ const openBigPicture = (pictureData) => {
   bigPicture.addEventListener('click', onOverlayClick);
 };
 
-export { openBigPicture };
+const onPictureClick = (evt, picturesData) => {
+  const pictureElement = evt.target.closest('.picture');
+  if (pictureElement) {
+    evt.preventDefault();
+    const pictureId = parseInt(pictureElement.dataset.pictureId, 10);
+    const pictureData = picturesData.find((item) => item.id === pictureId);
+    if (pictureData) {
+      openBigPicture(pictureData);
+    }
+  }
+};
+
+const initPictureHandlers = (picturesData) => {
+  picturesContainer.addEventListener('click', (evt) => {
+    onPictureClick(evt, picturesData);
+  });
+};
+
+export { openBigPicture, initPictureHandlers };
