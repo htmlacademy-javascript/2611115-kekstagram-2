aea13bcbed98ad00798675cb49a8f66e5a3bf739
Таисия <taisiya.kryachko@gmail.com>
2025-11-20 17:44:27 +0300

    //

--- js/main.js
+++ js/main.js
@@ -2,7 +2,20 @@ import { initPictureHandlers } from './big-picture.js';
 import { generatePhotos } from './data.js';
 import { renderThumbnails } from './thumbnails.js';
 import './form.js';
-const photos = generatePhotos();
-renderThumbnails(photos);
-initPictureHandlers(photos);
+import { getData } from './fetch.js';
+
+const loadPhotos = async () => {
+  try {
+    const photos = await getData();
+    renderThumbnails(photos);
+    initPictureHandlers(photos);
+  } catch (error) {
+
+    const photos = generatePhotos();
+    renderThumbnails(photos);
+    initPictureHandlers(photos);
+  }
+};
+loadPhotos();
+
 
